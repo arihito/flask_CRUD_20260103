3a7372e96ee12b7b9a8424b596204f677c0b5ee2
kenshu <kenshu.app@gmail.com>
2026-01-04 15:58:16 +0900

    add: relation & current display

--- Memo/memo/views.py
+++ Memo/memo/views.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models import db, Memo
 from forms import MemoForm
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 memo_bp = Blueprint('memo', __name__, url_prefix='/memo')
 
 @memo_bp.route('/')
 @login_required
 def index():
-	memos = Memo.query.all() # メモ全件取得
+	memos = Memo.query.filter_by(user_id=current_user.id).all() # メモ全件取得
 	return render_template('memo/index.j2', memos=memos)
 
 @memo_bp.route('/create', methods=['GET', 'POST'])
@@ -18,7 +18,7 @@ def create():
   if form.validate_on_submit():
     title = form.title.data
     content = form.content.data
-    memo = Memo(title=title, content=content)
+    memo = Memo(title=title, content=content, user_id=current_user.id)
     db.session.add(memo)
     db.session.commit()
     flash('登録しました')
@@ -28,7 +28,7 @@ def create():
 @memo_bp.route('/update/<int:memo_id>', methods=['GET', 'POST'])
 @login_required
 def update(memo_id):
-  target_data = Memo.query.get_or_404(memo_id) # 見つからない場合は404エラー
+  target_data = Memo.query.filter_by(id=memo_id, user_id=current_user.id).first_or_404() # 見つからない場合は404エラー
   form = MemoForm(obj=target_data)
   if request.method == 'POST' and form.validate():
     target_data.title = form.title.data
@@ -41,7 +41,7 @@ def update(memo_id):
 @memo_bp.route('/delete/<int:memo_id>')
 @login_required
 def delete(memo_id):
-  memo = Memo.query.get_or_404(memo_id)
+  memo = Memo.query.filter_by(id=memo_id, user_id=current_user.id).first_or_404()
   db.session.delete(memo)
   db.session.commit()
   flash('変更しました')
--- Memo/models.py
+++ Memo/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.orm import relationship
 db = SQLAlchemy() # Flaskのappと未連携の状態で空インスタンスを生成
 
 class Memo(db.Model):
@@ -8,12 +9,19 @@ class Memo(db.Model):
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	title = db.Column(db.String(50), nullable=False)
 	content = db.Column(db.Text) # 文字数無制限
+ 	# MemoのUser_idカラムはUserモデルのidカラムを外部キーとして設定
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_momos_users'), nullable=False) # 必ず紐付くことを保証
+	# MemoモデルはUserとリレーションシップを定義するためUserモデルのmemosを指定
+	user = relationship('User', back_populates='memos')
 
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(50), unique=True, nullable=False)
 	password = db.Column(db.String(120), nullable=False)
+  # UserモデルはMemoとリレーションシップを定義するためMemoモデルのuserを指定
+	memos = relationship('Memo', back_populates='user')
+
 	# ハッシュ化したパスワードを上記のカラムに保持
 	def set_password(self, password):
 		self.password = generate_password_hash(password)
