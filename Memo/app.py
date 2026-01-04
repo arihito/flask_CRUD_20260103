17f584e23f9795d77871d4a1251821b9c5087e53
kenshu <kenshu.app@gmail.com>
2026-01-04 14:12:25 +0900

    refactor: Blueprint auth memo

--- Memo/app.py
+++ Memo/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_migrate import Migrate
 from models import db, User
 from flask_login import LoginManager
+from auth.views import auth_bp
+from memo.views import memo_bp
+
 app = Flask(__name__)
 
 # 設定ファイルからConfigクラスを読み込み
@@ -15,7 +18,10 @@ login_manager = LoginManager()
 login_manager.init_app(app) # Flaskアプリに紐付け
 # アクセス制限ページからリダイレクト後のflashメッセージ
 login_manager.login_message = '認証していません：ログインしてください'
-login_manager.login_view = 'login' # 不正アクセスはログインページへ
+login_manager.login_view = 'auth.login' # 不正アクセスはログインページへ
+# Blueprintをアプリに紐付け
+app.register_blueprint(auth_bp)
+app.register_blueprint(memo_bp)
 
 @login_manager.user_loader
 def load_user(user_id):
